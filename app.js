649e9033d401d604144aed1e4e36bfae9858fae3
Salvador Arrazola <salvador.arrazola@gmail.com>
2024-02-07 13:50:09 -0600

    Agrega ejercicios de la clase 4.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-// Inicializamos el numero secreto con un valor especifico.
-let numeroSecreto = 6;
+// Inicializamos el numero secreto con un valor aleatorio entre 1 y 10.
+let numeroSecreto = Math.floor(Math.random() * 10) + 1;
 // Inicializamos el numero del usuario.
 let numeroUsuario = 0;
 // Inicializamos el numero de intentos del usuario.
 let numeroIntentos = 1;
-// Inicializamos la palabra para "intento" o "intentos".
-let palabraIntentos = 'intento';
+// Inicializamos el numero maximo de intentos.
+let numeroMaximoIntentos = 3;
 
 // Desplegamos un mensaje de bienvenida.
 alert('¡Bienvenido al juego de Adivina el Numero Secreto!');
@@ -14,20 +14,30 @@ alert('¡Bienvenido al juego de Adivina el Numero Secreto!');
 while (numeroUsuario != numeroSecreto) {
 
     // Solicitamos un numero al usuario.
-    let numeroUsuario = prompt('Por favor, dame un número entre el 1 y 10:');
+    numeroUsuario = prompt('Por favor, dame un número entre el 1 y 10:');
 
     // Usamos la consola para ver el numero secreto y el numero del usuario.
-    console.log('Numero Secreto: '+ numeroSecreto);
-    console.log('Numero Usuario: '+ numeroUsuario);
+    console.log('numeroSecreto: '+ numeroSecreto);
+    console.log('numeroUsuario: '+ numeroUsuario);
+    console.log('Tipo de dato de numeroUsuario: '+ typeof(numeroUsuario));
 
     // Checamos si el numero del usuario es igual al numero secreto.
     if (numeroUsuario == numeroSecreto) {
         // Si los numeros son iguales, felicitamos al usuario.
-        alert(`¡Felicidades!\nAdivinaste el Numero Secreto en ${numeroIntentos} ${palabraIntentos}: ${numeroSecreto}.`);
-        // Rompemos el ciclo para terminar el programa.
-        break;
+        // Usamos un operador ternario "?" para desplegar la palabra "intento" o "intentos"
+        // en base al valor de numeroIntentos.
+        alert(`¡Felicidades!\nAdivinaste el Numero Secreto en ${numeroIntentos} ${numeroIntentos == 1 ? 'intento' : 'intentos'}: ${numeroSecreto}.`);
     } else {
-        // Si no, checamos la relacion entre ambos numeros.
+        // Si no, checamos el numero de intentos.
+        if (numeroIntentos >= numeroMaximoIntentos) {
+            // Si el usuario llego al numero maximo de intentos.
+            alert(`¡Lo siento!\nHas superado el numero máximo de intentos: ${numeroMaximoIntentos}.\nEl Numero Secreto era: ${numeroSecreto}.`);
+            // Rompemos el ciclo while para terminar el programa.
+            break;
+        }
+
+        // Si el usuario no ha llegado al numero maximo de intentos,
+        // checamos la relacion entre el numero secreto y el numero del usuario.
         if (numeroSecreto > numeroUsuario) {
             // Si el numero secreto es mayor al numero del usuario.
             alert(`¡Lo siento!\nEl Numero Secreto es mayor a ${numeroUsuario}.`);
@@ -39,8 +49,4 @@ while (numeroUsuario != numeroSecreto) {
 
     // Incrementamos el contador de intentos.
     numeroIntentos++;
-    // Dado que el usuario ya completo el primer intento,
-    // cambiamos la palabra "intento" por "intentos".
-    palabraIntentos = 'intentos';
-
 }
